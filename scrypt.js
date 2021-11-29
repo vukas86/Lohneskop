6d59b0bb92badd0cbe9612d3d9ba028aa17c418d
“vukas86” <vukasbit@gmail.com>
2021-11-29 23:10:53 +0100

    finishing with footer and some JS

--- scrypt.js
+++ scrypt.js
@@ -1,11 +1,7 @@
 const quoteTabs = document.querySelectorAll(".quote-tab-div");
 const qouteContainer = document.querySelector(".quote-tab");
 const qouteContent = document.querySelectorAll(".quote-content");
-const spanLink = document.getElementById("linkic");
-console.log(spanLink);
-const hovered = document.querySelectorAll(".inactive2");
-
-console.log(hovered);
+const nav = document.querySelector(".nav-links");
 
 qouteContainer.addEventListener("click", function (e) {
   const clicked = e.target.closest(".quote-tab-div");
@@ -19,4 +15,27 @@ qouteContainer.addEventListener("click", function (e) {
   document
     .querySelector(`.quote-content-${clicked.dataset.tab}`)
     .classList.remove("inactive");
+});
+
+const handleHover = function (e) {
+  if (e.target.classList.contains("nav-link")) {
+    const link = e.target;
+    const sibling = link.closest(".nav-links").querySelectorAll(".nav-link");
+
+    sibling.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+  }
+};
+
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+nav.addEventListener("mouseout", handleHover.bind(1));
+
+nav.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains("nav-link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
 });
