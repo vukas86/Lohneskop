14ad9c7eb3762f641680fc2c721a5e2c5ca962c7
“vukas86” <vukasbit@gmail.com>
2021-11-30 22:50:32 +0100

    intersection observation

--- scrypt.js
+++ scrypt.js
@@ -2,6 +2,11 @@ const quoteTabs = document.querySelectorAll(".quote-tab-div");
 const qouteContainer = document.querySelector(".quote-tab");
 const qouteContent = document.querySelectorAll(".quote-content");
 const nav = document.querySelector(".nav-links");
+const navSticky = document.querySelector(".nav");
+const scrollBtn = document.querySelector(".sticky-hidden");
+const quoteSection = document.querySelector(".quote");
+const projectSection = document.querySelector("#projects");
+const aboutSection = document.querySelector(".about");
 
 qouteContainer.addEventListener("click", function (e) {
   const clicked = e.target.closest(".quote-tab-div");
@@ -39,3 +44,62 @@ nav.addEventListener("click", function (e) {
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) navSticky.classList.add("sticky");
+  else navSticky.classList.remove("sticky");
+};
+const aboutObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: "-136.5px",
+});
+
+aboutObserver.observe(aboutSection);
+
+scrollBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (!e.target.closest(".sticky-hidden")) return;
+
+  const idLink = e.target.getAttribute("href");
+  console.log(idLink);
+
+  document.querySelector(idLink).scrollIntoView({ behavior: "smooth" });
+});
+
+const obserBtn = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) scrollBtn.style.visibility = "visible";
+  else scrollBtn.style.visibility = "hidden";
+};
+
+const scrollObserver = new IntersectionObserver(obserBtn, {
+  root: null,
+  threshold: 0,
+});
+
+scrollObserver.observe(aboutSection);
+
+// Lazy loading imgs
+const imgTargets = document.querySelectorAll("img[data-src]");
+
+const loadImg = function (entries, observer) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) return;
+
+    entry.target.src = entry.target.dataset.src;
+
+    entry.target.addEventListener("load", function () {
+      entry.target.classList.remove("blur-img");
+    });
+  });
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0.5,
+});
+
+imgTargets.forEach((img) => imgObserver.observe(img));
